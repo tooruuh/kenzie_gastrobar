45c0168d11314f3202fc04428a6ba8f22cf388b2
Lestar Angelo <lestarherminio@gmail.com>
2022-05-11 16:58:14 -0300

    Fix: correction of the stylization and responsiveness of the releases page

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.button`
 
-    width: 262px;
     height: 50px;
     border-radius: 8px ;
     filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
