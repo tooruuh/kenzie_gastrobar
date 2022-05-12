efdd4ea46f0a9ca1ae3cb10eb9568001c7cf3243
Wellington José <paivawellington23@gmail.com>
2022-05-12 12:57:06 -0300

    feat: added providers register

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { api } from "../../Services/api";
-import { useHistory } from "react-router-dom";
-import { toast } from "react-toastify";
+import { useRegister } from "../../Providers/register";
 
 function RegisterPage() {
-  const history = useHistory();
+
+  const {registerCompany} = useRegister()
+
   const schema = yup.object().shape({
     name: yup.string().required("Campo obrigatório!"),
     email: yup.string().email("Email Inválido").required("Campo obrigatório!"),
@@ -34,31 +34,7 @@ function RegisterPage() {
   } = useForm({ resolver: yupResolver(schema) });
 
   function onSubmitFunction(data) {
-    api.post("/register", data)
-    .then((response) => {
-      toast.success('Conta criada com sucesso! ', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        });
-      history.push("/login")
-    })
-    .catch((err) => {
-      console.log(err)
-      toast.error('Ocorreu um erro no registro!', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      })
-    })
+    registerCompany(data)
   }
   
 
--- src/Providers/index.js
+++ src/Providers/index.js
@@ -2,17 +2,20 @@ import { ModalProviders } from "./modal";
 import { TablesProvider } from "./tables";
 import { LoginProviders} from './login'
 import { AdminProviders } from "./admin";
+import { RegisterProviders } from "./register";
 
 export function Providers({ children }) {
   return (
     <TablesProvider>
-      <LoginProviders>
-       <ModalProviders>
-        <AdminProviders>  
-            {children}
-        </AdminProviders>
-       </ModalProviders>
-      </LoginProviders>
+      <RegisterProviders>
+        <LoginProviders>
+          <ModalProviders>
+            <AdminProviders>  
+                {children}
+            </AdminProviders>
+          </ModalProviders>
+        </LoginProviders>
+      </RegisterProviders>
     </TablesProvider>
   );
 }
