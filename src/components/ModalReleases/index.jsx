88e3e3056f22f070258e18c890dfe3ccde09189a
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-05-16 11:41:05 -0300

    feature: create feature ModalReleases

---
+++ src/components/ModalReleases/index.jsx
@@ -0,0 +1,16 @@
+// import { useModal } from "../../Providers/modal";
+import { Container } from "./style";
+import Header from "../Header";
+
+function ModalReleases() {
+  //   const { modalReleases, setModalReleases } = useModal();
+
+  return (
+    <>
+      <Header></Header>
+      <Container>teste</Container>
+    </>
+  );
+}
+
+export default ModalReleases;
---
+++ src/components/ModalReleases/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  background-color: #5b1206;
+  width: 280px;
+  height: 481px;
+`;
